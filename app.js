387f772d689b3d27dbdb5baad69d97c599d3ad4e
Joshi <Swapnil.Joshi@sungard.com>
2017-04-14 10:12:39 +0530

    name calculator changes

---
+++ app.js
@@ -0,0 +1,29 @@
+//iefe- immediately executed function execution
+(function()
+{
+  //allows not to use global variables accidently
+  'use strict';
+
+  angular.module('NameCalculator',[])
+
+  .controller('NameCalculatorController',function($scope) {
+    $scope.name = "";
+    $scope.totalValue = 0;
+
+    $scope.displayNumeric = function()
+    {
+      var totalNameValue = calculateNumericForString($scope.name);
+      $scope.totalValue = totalNameValue;
+    }
+
+    function calculateNumericForString(string)
+    {
+      var totalStringValue = 0;
+      for(var i=0; i< string.length;i++)
+      {
+        totalStringValue += string.charCodeAt(i);
+      }
+      return totalStringValue;
+    }
+  });
+})();
